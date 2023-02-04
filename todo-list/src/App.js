da03a995d5597ec90a454e779b2fabc62b5da0c3
lokeshjawale1996 <jawalelokesh2411@gmail.com>
2023-02-04 12:23:07 +0530

    save and clear functionality

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Navbar from './components/NavBar';
 import Todo from './components/Todo';
 
 function App() {
   return (
     <div className="App">
-     <h1 className='bg-red-400'>Hello Welcome to Todo App</h1>
+     {/* <h1 className='bg-red-400'>Hello Welcome to Todo App</h1> */}
+     <Navbar/>
      <Todo/>
     </div>
   );
--- todo-list/src/components/Todo.js
+++ todo-list/src/components/Todo.js
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 function Todo() {
   const [todos, setTodos] = useState([]);
   const [inputVal, setinputVal] = useState("");
   const [edit, setEdit] = useState(false);
   const [indexToEdit, setIndexToEdit] = useState(null);
+  const [timeVal, setTimeVal] = useState("");
 
-  console.log("hiiiiiiiiii", inputVal);
   const handleSubmit = (e) => {
     e.preventDefault();
+ 
+        // setTimeVal(time);
 
     if (inputVal === "") {
       return;
     }
+
+
     if (edit) {
       const newTodos = [...todos];
       newTodos[indexToEdit] = inputVal;
@@ -23,14 +27,43 @@ function Todo() {
     }
     setinputVal("");
   };
+  const handleInput = (e) => {
+   setinputVal(e.target.value);
+   var d = new Date();
+       let s = d.getSeconds();
+       let m = d.getMinutes();
+       let h = d.getHours();
+       setTimeVal(`${h}:${m}:${s}`)
+
+  }
+  
   const handleEditTodo = (index) => {
     setinputVal(todos[index]);
     setEdit(true);
     setIndexToEdit(index);
   };
+
   const handleDeleteTodo = (index) => {
     setTodos(todos.filter((_,ind) => ind !== index))
   };
+//On clicking Save Button 
+  const handleSaveTodos = () => {
+    localStorage.setItem("todos",JSON.stringify(todos));
+  }
+
+//On Clicking Clear Button
+const handleClearAllTodo = () => {
+setTodos([]);
+localStorage.removeItem("todos");
+}
+
+useEffect (() => {
+    const savedTodos = localStorage.getItem("todos");
+    if(savedTodos){
+        setTodos(JSON.parse(savedTodos));
+    }
+
+},[]);
   return (
     <div>
       {/* on submiiting form handleSubmit event fires */}
@@ -39,19 +72,32 @@ function Todo() {
           autoFocus
           type="text"
           value={inputVal}
-          onChange={(e) => setinputVal(e.target.value)}
+          onChange={handleInput}
         />
         <button type="submit">{edit ? "Update" : "Add"}</button>
       </form>
+      <div>
       <ul>
         {todos.map((todo, index) => (
           <li key={index} className="flex gap-x-4 justify-center">
-            <p>{todo}</p>
+            <p>
+                <span>{todo}
+                {/* <br/>{timeVal} */}
+                </span>
+            </p>
+            
+            {/* onClicking Edit and Delete we are executing handleEditTodo & handleDeleteTodo  */}
             <button onClick={() => handleEditTodo(index)}>Edit</button>
             <button onClick={() => handleDeleteTodo(index)}>Delete</button>
           </li>
         ))}
       </ul>
+      </div>
+      <div>
+        <button onClick={handleSaveTodos}>Save</button>
+        <button onClick={handleClearAllTodo}>Clear</button>
+      </div>
+   
     </div>
   );
 }
